6560c91eadd2fa277639a9ee55a48db07023550e
Ikenna Richard <oguejioforrichard@gmail.com>
2024-07-08 20:53:51 +0100

    Add shopping cart to checkout

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,19 +1,25 @@
-import React from 'react'
-import BillingForm from './_components/billing-form'
-import { Metadata } from 'next'
-import ShoppingCart from './_components/shopping-cart'
-
-export const metadata: Metadata = {
-  title: 'Checkout'
-}
+"use client";
+import BillingForm from "./_components/billing-form";
+import ShoppingCart from "./_components/shopping-cart";
+import { totalPayment } from "../lib/formatCurrency";
+import { useShoppingCart } from "../context/ShoppingCartContext";
+import { products } from "../lib/products-data";
 
 export default function CheckoutPage() {
+  const { cartItems } = useShoppingCart();
+  const total = totalPayment(
+    cartItems.reduce((total, cartItem) => {
+      const item = products.find((i) => i.id === cartItem.id);
+      return total + (item?.price || 0) * cartItem.quantity;
+    }, 0)
+  );
   return (
-    <main className='bg-light_cyan min-h-screen flex flex-col justify-center'>
-      <section className='flex flex-wrap justify-center gap-3 pt-12'>
-        <BillingForm/>
-        <ShoppingCart/>
+    <main className="bg-light_cyan min-h-screen flex flex-col justify-center">
+      <title>Checkout</title>
+      <section className="flex flex-wrap justify-center gap-3 pt-12">
+        <BillingForm amount={total} />
+        <ShoppingCart />
       </section>
     </main>
-    )
+  );
 }
