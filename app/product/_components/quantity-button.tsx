0274508b011e6a744f4d108b23a0fdd38cf32624
Ikenna Richard <oguejioforrichard@gmail.com>
2024-07-08 21:04:33 +0100

    Refactor code

--- app/product/_components/quantity-button.tsx
+++ app/product/_components/quantity-button.tsx
@@ -1,9 +1,33 @@
-export default function QuantityButton() {
+"use client";
+import { useShoppingCart } from "@/app/context/ShoppingCartContext";
+import { Product } from "@/app/lib/products-data";
+
+type QuantityButtonProp = {
+  productType: Product | string;
+};
+
+export default function QuantityButton({ productType }: QuantityButtonProp) {
+  const { decreaseCartQuantity, increaseCartQuantity, getItemQuantity } =
+    useShoppingCart();
+
+  const product = productType as Product;
+  const quantity = getItemQuantity(product.id);
+
   return (
-    <div className="bg-[#AEAEAE] flex items-center justify-around rounded-md w-1/5 px-1 py-1">
-      <button className="text-2xl">-</button>
-      <span className="text-reddish font-bold text-xl">1</span>
-      <button className="text-2xl">+</button>
+    <div className="bg-[#AEAEAE] flex items-center justify-around rounded-md w-[6rem] px-1 py-1">
+      <button
+        onClick={() => decreaseCartQuantity(product.id)}
+        className="text-2xl"
+      >
+        -
+      </button>
+      <span className="text-reddish font-bold text-xl">{quantity}</span>
+      <button
+        onClick={() => increaseCartQuantity(product.id)}
+        className="text-2xl"
+      >
+        +
+      </button>
     </div>
   );
 }
