d6f76684c5067bf4385028512b5c0f639b7185c3
Ikenna Richard <oguejioforrichard@gmail.com>
2024-07-08 21:01:56 +0100

    Remove links from social icons

--- app/component/footer/socials.tsx
+++ app/component/footer/socials.tsx
@@ -1,20 +1,13 @@
 import Facebook from "@/public/icons/facebook";
 import Instagram from "@/public/icons/instagram";
 import Pinterest from "@/public/icons/pinterest";
-import Link from "next/link";
 
 export default function Socials() {
   return (
     <div className="flex items-center gap-5 mx-auto">
-      <Link href="#">
-        <Pinterest />
-      </Link>
-      <Link href="#">
-        <Instagram />
-      </Link>
-      <Link href="#">
-        <Facebook />
-      </Link>
+      <Pinterest />
+      <Instagram />
+      <Facebook />
     </div>
   );
 }
