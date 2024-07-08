e1f2b2f645f6bfe1af8bf754e59589038fa14f93
Ikenna Richard <oguejioforrichard@gmail.com>
2024-07-08 21:01:29 +0100

    Refactor code

--- app/component/faq/FAQItem.tsx
+++ app/component/faq/FAQItem.tsx
@@ -2,7 +2,7 @@ import PlusIcon from "@/public/icons/plus-icon";
 
 export default function FAQItem({ question }: { question: string }) {
   return (
-    <li className="flex justify-between items-center capitalize border-2 border-[#00000088] p-6 shadow-md font-semibold mb-3">
+    <li className="flex justify-between items-center capitalize border-2 border-[#00000088] px-3 py-6 shadow-md font-semibold mb-3 text-[1.3rem]">
       {question} <PlusIcon />
     </li>
   );
