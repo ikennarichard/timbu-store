65c2299a99d454301cf7374bb5dfda191ec2631e
Ikenna Richard <oguejioforrichard@gmail.com>
2024-07-08 20:55:52 +0100

    Refactor code

--- app/component/AboutUs.tsx
+++ app/component/AboutUs.tsx
@@ -1,28 +1,28 @@
-import Image from "next/image"
+import Image from "next/image";
 
 export default function AboutUs() {
   return (
     <section
-        id="about"
-        className="text-center flex justify-center overflow-hidden items-center bg-[#0C8E8E] py-20 relative"
-      >
-        <div className="w-[70%] text-white z-10">
-          <h2 className="text-[2rem] font-bold">About us</h2>
-          <p className="text-[1.8rem] mt-6">
-            At Vault, we&apos;re passionate about fitness and helping our
-            customers achieve their goals. With years of experience in the
-            industry, we understand the importance of quality supplements in
-            maximizing performance and results.
-          </p>
-        </div>
-        <Image
-            src="/images/gym-guy.png"
-            alt="A muscular gym guy"
-            width={420}
-            height={200}
-            quality={80}
-            className="absolute right-28 -bottom-3"
-          />
-      </section>
-  )
+      id="about"
+      className="text-center flex justify-center overflow-hidden items-center bg-[#0C8E8E] text-pretty pt-10 pb-[8rem] relative"
+    >
+      <div className="w-[65%] text-white z-10">
+        <h2 className="text-[2rem] font-bold">About us</h2>
+        <p className="text-[2rem] mt-12 word">
+          At Vault, we&apos;re passionate about fitness and helping our
+          customers achieve their goals. With years of experience in the
+          industry, we understand the importance of quality supplements in
+          maximizing performance and results.
+        </p>
+      </div>
+      <Image
+        src="/images/gym-guy.png"
+        alt="A muscular gym guy"
+        width={500}
+        height={0}
+        quality={80}
+        className="absolute right-10 -bottom-3"
+      />
+    </section>
+  );
 }
