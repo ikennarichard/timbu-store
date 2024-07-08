a1cade3533dc5b574e97225739050bc1151e0087
Ikenna Richard <oguejioforrichard@gmail.com>
2024-07-08 08:21:28 +0100

    Add the hero section

---
+++ app/component/Hero.tsx
@@ -0,0 +1,21 @@
+import Button from "./Button"
+
+export default function Hero() {
+  return (
+    <section className="relative grid place-content-center w-full min-h-screen bg-[url('/images/hero-bg.svg')] bg-cover bg-no-repeat">
+    <div className="z-10">
+      <h2 className="capitalize text-white text-[4.625rem] text-center font-extrabold">
+        fuel your fitness <span className="text-[#E5C207]">journey</span>{" "}
+        <p>
+          <span className="lowercase">with</span> premium suppliments
+        </p>
+      </h2>
+      <div className="flex justify-center gap-6">
+        <Button text="View Product" link="#" />
+        <Button text="Explore" link="#" />
+      </div>
+    </div>
+    <div className="absolute inset-0 bg-black opacity-50"></div>
+  </section>
+  )
+}
