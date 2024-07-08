09c5cc71dacaadfd640f35c6d5afdbe5de9a1bd8
Ikenna Richard <oguejioforrichard@gmail.com>
2024-07-08 08:19:56 +0100

    Add custom button

---
+++ app/component/Button.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+import clsx from "clsx";
+
+export default function Button({ text, link }: { text: string; link: string }) {
+  return (
+    <button
+      className={clsx(
+        "p-3 bg-blue-700 text-white rounded-md hover:bg-reddish transition-colors duration-300 ease-in-out"
+      )}
+    >
+      <Link href={link}>{text}</Link>
+    </button>
+  );
+}
