edae9eded3b864a7bcf942c317323c4e485d0055
Ikenna Richard <oguejioforrichard@gmail.com>
2024-07-08 08:11:00 +0100

    Update icons and images

---
+++ public/icons/plus-icon.tsx
@@ -0,0 +1,16 @@
+export default function PlusIcon() {
+  return (
+    <svg
+      width="16"
+      height="16"
+      viewBox="0 0 30 30"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M29.5837 17.0793H17.0837V29.5793H12.917V17.0793H0.416992V12.9126H12.917V0.412598H17.0837V12.9126H29.5837V17.0793Z"
+        fill="black"
+      />
+    </svg>
+  );
+}
