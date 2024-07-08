d7985c72a63b3488d4eeb3eb244a39bd24d2cb52
Ikenna Richard <oguejioforrichard@gmail.com>
2024-07-08 20:54:56 +0100

    Add payment formatter

--- app/lib/formatCurrency.ts
+++ app/lib/formatCurrency.ts
@@ -4,6 +4,16 @@ const CURRENCY_FORMATTER = new Intl.NumberFormat('en-US', {
   minimumFractionDigits: 0,
 })
 
+const PAYMENT_FORMATTER = new Intl.NumberFormat('en-US', {
+  currency: 'USD', 
+  style: 'currency',
+  minimumFractionDigits: 1,
+})
+
 export function formatCurrency(number: number) {
   return CURRENCY_FORMATTER.format(number);
+}
+
+export function totalPayment(number: number) {
+  return PAYMENT_FORMATTER.format(number)
 }
