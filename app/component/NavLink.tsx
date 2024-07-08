35059cded6fedc4d7232d149ce41b42eaa709c12
Ikenna Richard <oguejioforrichard@gmail.com>
2024-07-08 08:15:12 +0100

    Add navlinks

---
+++ app/component/NavLink.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import clsx from "clsx";
+
+const links = [
+  { name: "Products", href: "/" },
+  { name: "Pricing", href: "/pricing" },
+  { name: "About us", href: "/#about" },
+  { name: "FAQ", href: "/#faq" },
+];
+
+export default function NavLinks() {
+  const pathname = usePathname();
+  return (
+    <>
+      {links.map((link) => {
+        return (
+          <li key={link.name} className="list-none">
+            <Link
+              href={link.href}
+              className={clsx(
+                "",
+                {
+                  "text-black font-bold": pathname === link.href,
+                }
+              )}
+            >
+              {link.name}
+            </Link>
+          </li>
+        );
+      })}
+    </>
+  );
+}
