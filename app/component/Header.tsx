2db5ea21091aa8bb914c3c3df672ba77847b38ed
Ikenna Richard <oguejioforrichard@gmail.com>
2024-07-08 20:57:44 +0100

    Refactor code

--- app/component/Header.tsx
+++ app/component/Header.tsx
@@ -1,12 +1,12 @@
 'use client'
 
-import React from "react";
 import NavBar from "./NavBar";
 import Button from "./Button";
-import { PhShoppingCartSimpleFill } from "./CartIcon";
+import { CartIcon } from "./CartIcon";
 import Logo from "./Logo";
 import clsx from "clsx";
 import { usePathname } from "next/navigation";
+import { FormkitAvatarman } from "@/public/icons/avatar";
 
 export default function Header() {
   const pathname = usePathname()
@@ -15,10 +15,12 @@ export default function Header() {
     <header className={clsx("bg-[#ffffffd3] z-10 rounded-lg flex gap-[70px] w-11/12 items-center justify-between px-3 py-4 mx-auto", pathname === '/' && "absolute top-6 left-[4%]")}>
       <Logo textColor="black" />
       <NavBar />
-      <div className="flex items-center gap-3 relative">
-        <Button text="Login" link="#" />
-        <Button text="Sign Up" link="#" />
-        <PhShoppingCartSimpleFill />
+      <div className="flex items-center gap-3">
+        <CartIcon />
+        <div className="flex items-center gap-2 ml-2">
+          <FormkitAvatarman />
+          <p className="text-xl">Account</p>
+        </div>
       </div>
     </header>
   );
