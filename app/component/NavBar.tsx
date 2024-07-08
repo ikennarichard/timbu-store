2f7746aaad320adefcbcf79ad7bfa97b6e1cf166
Ikenna Richard <oguejioforrichard@gmail.com>
2024-07-08 08:21:04 +0100

    Add navbar

---
+++ app/component/NavBar.tsx
@@ -0,0 +1,11 @@
+import NavLinks from './NavLink'
+
+export default function NavBar() {
+  return (
+    <nav className='flex items-center'>
+      <ul className='flex gap-6'>
+        <NavLinks/>
+      </ul>
+    </nav>
+  )
+}
