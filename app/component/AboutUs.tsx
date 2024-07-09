c5af137e5504636b6796badcdaa3d220015e1a7b
Ikenna Richard <oguejioforrichard@gmail.com>
2024-07-09 16:31:54 +0100

    Update styles

--- app/component/AboutUs.tsx
+++ app/component/AboutUs.tsx
@@ -4,11 +4,11 @@ export default function AboutUs() {
   return (
     <section
       id="about"
-      className="text-center flex justify-center overflow-hidden items-center bg-[#0C8E8E] text-pretty pt-10 pb-[8rem] relative"
+      className="text-center flex justify-center overflow-hidden items-center bg-[#0C8E8E] text-pretty pt-10 pb-[8rem] relative md:bg-transparent md:absolute md:min-h-screen sm:animate-fade w-full"
     >
-      <div className="w-[65%] text-white z-10">
-        <h2 className="text-[2rem] font-bold">About us</h2>
-        <p className="text-[2rem] mt-12 word">
+      <div className="w-[65%] sm:w-[98%] text-white mx-auto md:relative md:top-[17rem]">
+        <h2 className="text-[2rem] font-bold sm:text-2xl">About us</h2>
+        <p className="text-[2rem] max-w-[25rem] md:w-[88%] md:mx-auto mt-12 word md:text-[0.875rem] md:mt-[1.2rem]">
           At Vault, we&apos;re passionate about fitness and helping our
           customers achieve their goals. With years of experience in the
           industry, we understand the importance of quality supplements in
@@ -21,7 +21,7 @@ export default function AboutUs() {
         width={500}
         height={0}
         quality={80}
-        className="absolute right-10 -bottom-3"
+        className="absolute right-10 -bottom-3 md:hidden"
       />
     </section>
   );
--- app/component/Button.tsx
+++ app/component/Button.tsx
@@ -5,10 +5,12 @@ export default function Button({ text, link }: { text: string; link: string }) {
   return (
     <button
       className={clsx(
-        "px-8 font-semibold p-3 bg-blue-700 text-white rounded-md hover:bg-reddish transition-colors duration-300 ease-in-out"
+        "px-8 sm:px-[20px] font-semibold p-3 bg-blue-700 text-white rounded-md hover:bg-reddish transition-colors duration-300 ease-in-out"
       )}
     >
-      <Link href={link}>{text}</Link>
+      <Link className="sm:text-[0.9rem]" href={link}>
+        {text}
+      </Link>
     </button>
   );
 }
