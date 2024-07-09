eace115a4d4cd207a8193ebddbf9954be61fd165
Ikenna Richard <oguejioforrichard@gmail.com>
2024-07-09 16:28:21 +0100

    Update custom tailwind configs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,7 +14,25 @@ const config: Config = {
         light_cyan: "#E0F7FA",
         alabaster: "#E8F5E9",
         laurel_green: "#BDC09F",
-        light_silver: "#D9D9D9"
+        light_silver: "#D9D9D9",
+        dark_charcoal: "#333333",
+      },
+
+      screens: {
+        "x-sm": { max: "640px" },
+        sm: { max: "900px" },
+        md: { max: "1024px" },
+      },
+
+      keyframes: {
+        fade: {
+          "0%": { opacity: "0.5", transform: "translateX(3px)", scale: "0.8" },
+          "100%": { opacity: "1", transform: "translateX(0)", scale: "1" },
+        },
+      },
+
+      animation: {
+        fade: "fade 2s ease-in-out",
       },
     },
   },
