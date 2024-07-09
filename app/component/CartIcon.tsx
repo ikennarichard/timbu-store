f16e553dbbc59827b4f95731e32e4fd5961646ab
Ikenna Richard <oguejioforrichard@gmail.com>
2024-07-09 16:33:03 +0100

    Update fill color for small screens

--- app/component/CartIcon.tsx
+++ app/component/CartIcon.tsx
@@ -4,7 +4,7 @@ import { useShoppingCart } from "../context/ShoppingCartContext";
 export function CartIcon() {
   const { cartQuantity } = useShoppingCart();
   return (
-    <div className="relative w-10 h-10 rounded-full bg-laurel_green flex items-center justify-center">
+    <div className="relative w-10 h-10 rounded-full bg-laurel_green flex items-center justify-center sm:rounded-md sm:bg-dark_charcoal">
       <Link href="/checkout">
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -14,6 +14,7 @@ export function CartIcon() {
         >
           <path
             fill="#000"
+            className="sm:fill-white"
             d="m239.71 74.14l-25.64 92.28A24.06 24.06 0 0 1 191 184H92.16A24.06 24.06 0 0 1 69 166.42L33.92 40H16a8 8 0 0 1 0-16h24a8 8 0 0 1 7.71 5.86L57.19 64H232a8 8 0 0 1 7.71 10.14M88 200a16 16 0 1 0 16 16a16 16 0 0 0-16-16m104 0a16 16 0 1 0 16 16a16 16 0 0 0-16-16"
           ></path>
         </svg>
